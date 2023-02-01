9e43cea24686c8c2705d7cc836c9c2aebe84b625
wahaj177 <wahajbutt2022@gmail.com>
2023-02-01 17:38:10 +0500

    changws

--- lib/main.dart
+++ lib/main.dart
@@ -95,6 +95,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            ConstrainedBox(constraints: constraints);
             const Text(
               'You have pushed the button this many times:',
             ),
